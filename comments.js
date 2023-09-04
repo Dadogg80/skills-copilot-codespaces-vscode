aa496526545a7180539f9bb5b71cd1df469bd3eb
Ivo G <41997352+Dadogg80@users.noreply.github.com>
2023-09-04 13:36:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,23 @@
-// Create web server
+// Create web server application
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// Import express
+const express = require('express');
 
-var server = http.createServer(function(req, res) {
-  var path = url.parse(req.url).pathname;
-  console.log(path);
-  switch (path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('hello world');
-      res.end();
-      break;
-    case '/socket.html':
-      fs.readFile(__dirname + path, function(err, data) {
-        if (err) {
-          return send404(res);
-        }
-        res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-        res.write(data, 'utf8');
-        res.end();
-      });
-      break;
-    default:
-      send404(res);
-  }
-});
+// Import router
+const router = express.Router();
 
-function send404(res) {
-  res.writeHead(404);
-  res.write('404');
-  res.end();
-}
+// Import comment controller
+const commentController = require('../controllers/commentController');
 
-server.listen(8080);
-console.log('server running at http://localhost:8080/');
+// Import middleware
+const auth = require('../middleware/auth');
+const upload = require('../middleware/upload');
+
+// Create routes
+router.post('/create', auth, upload.single('image'), commentController.createComment);
+router.get('/read', auth, commentController.readComment);
+router.put('/update/:id', auth, commentController.updateComment);
+router.delete('/delete/:id', auth, commentController.deleteComment);
+
+// Export router
+module.exports = router;
